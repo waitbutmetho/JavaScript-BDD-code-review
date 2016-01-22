85d1123053ee1f96a95b86bc64adb3b5cd3ebbf3
Joshua Gustafson <silversevenfox@yahoo.com>
2016-01-22 14:25:34 -0800

    add third spec

--- spec/specs.js
+++ spec/specs.js
@@ -5,4 +5,7 @@ describe("pingPong", function(){
   it("return numbers from 1 to the number input, but replace all numbers divisible by three with ping, and all numbers divisible by 5 with pong", function() {
     expect(pingPong(5)).to.eql([1, 2, "ping", 4, "pong"]);
   });
+  it("return numbers from 1 to the number input, but replace all numbers divisible 3, 5, and 15 with ping, pong, and pingpong", function() {
+    expect(pingPong(15)).to.eql([1, 2, "ping", 4, "pong", "ping", 7, 8, "ping", "pong", 11, "ping", 13, 14, "pingpong"]);
+  });
 });

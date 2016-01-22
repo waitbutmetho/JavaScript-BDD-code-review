4c2633d64a8bd8f326b9377c3758e7d638ce45f7
Joshua Gustafson <silversevenfox@yahoo.com>
2016-01-22 14:07:36 -0800

    add new spec for pingPong

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
-var resultArray = [];
 pingPong = function(input) {
+  var resultArray = [];
   for (var index = 1; index <= input; index++) {
-    if (index % 3 !== 0) {
+    if (index % 3 !== 0 && index % 5 !== 0) {
       resultArray.push(index)
     }
     else if (index % 3 === 0){
--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,7 @@ describe("pingPong", function(){
   it("return numbers from 1 to the number input, but replace all numbers divisible by three with ping", function() {
     expect(pingPong(3)).to.eql([1, 2, "ping"]);
   });
+  it("return numbers from 1 to the number input, but replace all numbers divisible by three with ping, and all numbers divisible by 5 with pong", function() {
+    expect(pingPong(5)).to.eql([1, 2, "ping", 4, "pong"]);
+  });
 });

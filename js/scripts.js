bc4ac39a4399e24cdc231c903847a8f19c01aa8b
Joshua Gustafson <silversevenfox@yahoo.com>
2016-01-22 13:43:27 -0800

    wrote script to pass fisrt spec

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,12 @@
-
+var resultArray = [];
+pingPong = function(input) {
+  for (var index = 1; index <= input; index++) {
+    if (index % 3 !== 0) {
+      resultArray.push(index)
+    }
+    else if (index % 3 === 0){
+      resultArray.push("ping");
+    }
+  };
+  return resultArray;
+};

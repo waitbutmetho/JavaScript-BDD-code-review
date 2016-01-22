c4df8c6d8aeb2a725b8f7197734255783d394436
Joshua Gustafson <silversevenfox@yahoo.com>
2016-01-22 14:29:13 -0800

    add js to pass third spec

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,11 @@
 pingPong = function(input) {
   var resultArray = [];
   for (var index = 1; index <= input; index++) {
-    if (index % 3 !== 0 && index % 5 !== 0) {
+    if (index % 3 !== 0 && index % 5 !== 0 && index % 15 !== 0) {
       resultArray.push(index)
+    }
+    else if (index % 15 === 0){
+      resultArray.push("pingpong");
     }
     else if (index % 5 === 0){
       resultArray.push("pong");
